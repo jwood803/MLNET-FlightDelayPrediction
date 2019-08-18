c8cf045777a2c84cd591b0ed383730b62800139c
J W <woodjon@gmail.com>
2019-08-18 14:10:51 -0400

    Update model

--- FlightDelayPrediction/FlightDelayPrediction/Program.cs
+++ FlightDelayPrediction/FlightDelayPrediction/Program.cs
@@ -1,6 +1,9 @@
 ﻿using FlightDelayPrediction.DataModels;
 using Microsoft.ML;
+using Microsoft.ML.Data;
 using System;
+using System.Linq;
+using static Microsoft.ML.Transforms.MissingValueReplacingEstimator;
 
 namespace FlightDelayPrediction
 {
@@ -10,14 +13,35 @@ namespace FlightDelayPrediction
         {
             var context = new MLContext();
 
-            var data = context.Data.LoadFromTextFile<FlightDelayData>("./FlightData.csv", hasHeader: true, separatorChar: ',');
+            var data = context.Data.LoadFromTextFile<FlightDelayData>("./FlightData.csv", hasHeader: true, separatorChar: ',', allowQuoting: true);
 
-            var keepColumnsTransform = context.Transforms.SelectColumns(
-                "MONTH", "DAY_OF_MONTH", "DAY_OF_WEEK", "ORIGIN", "DEST", "CRS_DEP_TIME", "ARR_DEL15");
+            var dataPrepPipeline = context.Transforms.SelectColumns("MONTH", "DAY_OF_MONTH", "DAY_OF_WEEK", "ORIGIN", "DEST", "CRS_DEP_TIME", "ARR_DEL15")
+                .Append(context.Transforms.Categorical.OneHotEncoding("OriginEncoded", "ORIGIN"))
+                .Append(context.Transforms.Categorical.OneHotEncoding("DestEncoded", "DEST"))
+                .Append(context.Transforms.DropColumns("ORIGIN", "DEST"))
+                .Append(context.Transforms.Concatenate("Features", "MONTH", "DAY_OF_MONTH", "DAY_OF_WEEK", "CRS_DEP_TIME", "OriginEncoded", "DestEncoded"))
+                .Append(context.Transforms.CopyColumns("Label", "ARR_DEL15"))
+                .Append(context.Transforms.Conversion.ConvertType("Label", "Label", DataKind.Boolean));
 
-            var newData = keepColumnsTransform.Fit(data).Transform(data);
+            data = dataPrepPipeline.Fit(data).Transform(data);
 
-            ConsoleHelper.PeekData(newData);
+            ConsoleHelper.PeekData(data);
+
+            var fillNaTransform = context.Transforms.ReplaceMissingValues("ARR_DEL15Replaced", "ARR_DEL15", ReplacementMode.DefaultValue);
+            data = fillNaTransform.Fit(data).Transform(data);
+
+            var testTrainData = context.Data.TrainTestSplit(data, testFraction: 0.2);
+
+            var trainer = context.BinaryClassification.Trainers.SdcaLogisticRegression();
+            var model = trainer.Fit(testTrainData.TrainSet);
+
+            var predictions = model.Transform(testTrainData.TestSet);
+
+            var metrics = context.BinaryClassification.Evaluate(predictions);
+
+            Console.WriteLine($"AUC - {metrics.AreaUnderRocCurve}");
+
+            Console.ReadLine();
         }
     }
 }
